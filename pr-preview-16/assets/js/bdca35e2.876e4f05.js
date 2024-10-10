41bbe30cd5b03c090ed5524393fec5add305bc8f
iliasmavridis <imavridis@anynines.com>
2024-10-11 01:22:19 +0200

    remove old previews

--- pr-preview-16/assets/js/bdca35e2.876e4f05.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4204],{7120:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/test2/pr-preview-16/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- pr-preview-17/assets/js/d5cdb045.86e8611b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2324],{8580:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/test2/pr-preview-17/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
