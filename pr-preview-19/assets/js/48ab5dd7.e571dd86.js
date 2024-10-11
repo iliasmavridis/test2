9826e4aa49ba6bb4cdd5e78c3dec8be2b8f949cd
iliasmavridis <imavridis@anynines.com>
2024-10-11 10:06:10 +0200

    remove old pr previes

--- pr-preview-19/assets/js/48ab5dd7.e571dd86.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4062],{5226:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/test2/pr-preview-19/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- pr-preview-21/assets/js/96ec09d1.041daa42.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7670],{5124:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/test2/pr-preview-21/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
