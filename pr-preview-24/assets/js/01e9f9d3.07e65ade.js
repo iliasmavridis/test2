061ee926c8f6705a58cf1f98fad1d2c2d1ba3f7b
iliasmavridis <imavridis@anynines.com>
2024-10-11 10:26:02 +0200

    remove old feature folder

--- pr-preview-24/assets/js/01e9f9d3.07e65ade.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7872],{131:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/test2/pr-preview-24/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
